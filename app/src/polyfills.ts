df972a61c831b35de886d6586f730972da9a0306
SondreB <sondre@outlook.com>
2024-06-12 00:20:36 +0200

    Fix polyfill issue

--- app/src/polyfills.ts
+++ app/src/polyfills.ts
@@ -3,5 +3,7 @@ import * as crypto from 'crypto';
 import * as stream from 'stream';
 
 (window as any).process = process;
-(window as any).crypto = crypto;
+if (!(window as any).crypto) {
+    (window as any).crypto = crypto;
+  }
 (window as any).stream = stream;
