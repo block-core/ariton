8af04d569b6dc7475b292946371d4031999b50ed
SondreB <sondre@outlook.com>
2024-06-11 14:41:16 +0200

    Add webpack and polyfills

--- app/src/polyfills.ts
+++ app/src/polyfills.ts
@@ -1,3 +1,7 @@
 import * as process from 'process';
+import * as crypto from 'crypto';
+import * as stream from 'stream';
 
 (window as any).process = process;
+(window as any).crypto = crypto;
+(window as any).stream = stream;
---
+++ app/src/webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+
+module.exports = {
+    resolve: {
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+        },
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new webpack.NormalModuleReplacementPlugin(
+            /node:crypto/,
+            require.resolve('crypto-browserify')
+        ),
+        new webpack.DefinePlugin({
+            global: 'globalThis',
+        }),
+    ],
+};
