1243ceafe076b94b996d6582c10182570a00fa4a
SondreB <sondre@outlook.com>
2024-06-17 21:52:02 +0200

    Add basic toolbox for communities

--- app/src/app/communities/communities.component.ts
+++ app/src/app/communities/communities.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-communities',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonToggleModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './communities.component.html',
-  styleUrl: './communities.component.scss'
+  styleUrl: './communities.component.scss',
 })
 export class CommunitiesComponent {
+  search = new FormControl('');
+  view = new FormControl('card');
 
 }
