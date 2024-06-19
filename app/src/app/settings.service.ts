36780c63a48de4987bb7283a46f5b93d9297bfc2
SondreB <sondre@outlook.com>
2024-06-19 14:46:24 +0200

    Implement a Signals based service

---
+++ app/src/app/settings.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { SignalsSimpleStoreService } from './signals.service';
+
+export interface SettingsState {
+  language: string;
+  debug: boolean;
+  servers: string[];
+}
+
+@Injectable()
+export class SettingsStateService extends SignalsSimpleStoreService<SettingsState> {
+  constructor() {
+    super();
+  }
+}
--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SettingsState, SettingsStateService } from '../settings.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SettingsStateService]
 })
 export class SettingsComponent {
 
+  debug = this.settingsState.select('debug');
+
+  readonly settings = this.settingsState.state.asReadonly();
+
+  constructor(private settingsState: SettingsStateService) {
+
+  }
+
+  toggleDebug() {
+    // Update single property
+    //this.settingsState.set('debug', !this.settings().debug);
+
+     // Update full or partial state
+     this.settingsState.setState({ debug: !this.settings().debug } as SettingsState);
+  }
 }
