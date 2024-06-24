a1016f1daf6d216721e3ce84632b2a86360502ae
SondreB <sondre@outlook.com>
2024-06-24 11:45:35 +0200

    Add links in Settings

--- app/src/app/settings/settings.component.ts
+++ app/src/app/settings/settings.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SettingsState, SettingsStateService } from '../settings.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatListModule, MatIconModule, RouterLink],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
