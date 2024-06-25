7727a369f7f8cb8e0681fd403ea58a21df50f800
SondreB <sondre@outlook.com>
2024-06-25 23:19:42 +0200

    Complete the unlock component

--- app/src/app/account/unlock/unlock.component.ts
+++ app/src/app/account/unlock/unlock.component.ts
@@ -1,18 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {Validators} from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { Validators } from '@angular/forms';
 import { IdentityService } from '../../identity.service';
-import { Web5IdentityAgent } from '@web5/identity-agent';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-unlock',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [FormsModule, MatProgressSpinnerModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './unlock.component.html',
   styleUrl: './unlock.component.scss'
 })
@@ -21,18 +21,27 @@ export class UnlockComponent {
 
   hide = signal(true);
 
+  unlocking = signal(false);
+
   invalidPassword = signal(false);
 
-  constructor(private identityService: IdentityService) {}
+  loading = false;
 
-  /*
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide);
-    event.stopPropagation();
-  }*/
+  passwordSignal = toSignal(this.passwordInput.valueChanges, { initialValue: '' });
+
+  disabled = computed(() => {
+    console.log('computed');
+    return this.passwordInput.invalid;
+  });
+
+  constructor(private identityService: IdentityService) { }
+
+  save() {
+    this.loading = true;
+  }
 
   async onSubmit() {
-    console.log('ON SUBMIT!');
+    this.unlocking.set(true);
     const unlocked = await this.identityService.unlock(this.passwordInput.value!);
 
     if (unlocked) {
@@ -42,5 +51,7 @@ export class UnlockComponent {
       console.log('Failed to unlock vault');
       this.invalidPassword.set(true);
     }
+
+    this.unlocking.set(false);
   }
 }
