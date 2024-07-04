1be62472f8559af845a8e57b8401d7cb43c5ec4d
SondreB <sondre@outlook.com>
2024-07-04 23:28:05 +0200

    Add about page

--- app/src/app/settings/about/about.component.ts
+++ app/src/app/settings/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
