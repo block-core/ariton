5fd1219d8b29358910db10777f1d1e8d7e113b40
SondreB <sondre@outlook.com>
2024-07-09 01:26:25 +0200

    Start on the restore component

--- app/src/app/account/create/restore/restore.component.ts
+++ app/src/app/account/create/restore/restore.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
     selector: 'app-restore',
     standalone: true,
-    imports: [],
+    imports: [MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatRadioModule, MatCardModule, ReactiveFormsModule],
     templateUrl: './restore.component.html',
     styleUrl: './restore.component.scss',
 })
-export class RestoreComponent {}
+export class RestoreComponent {
+    private fb = inject(FormBuilder);
+
+    addressForm = this.fb.group({
+        recoveryPhrase: [null, Validators.required],
+    });
+
+    onSubmit(): void {
+        console.log(this.addressForm.controls.recoveryPhrase.value);
+        alert('Thanks!');
+    }
+}
