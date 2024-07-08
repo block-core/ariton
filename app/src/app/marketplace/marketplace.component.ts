5dd055245d2f892f69cc8020f4dfcfdc4e6293e6
SondreB <sondre@outlook.com>
2024-07-08 16:27:12 +0200

    Add a basic home for Marketplace

--- app/src/app/marketplace/marketplace.component.ts
+++ app/src/app/marketplace/marketplace.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-marketplace',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatCardModule, RouterLink],
   templateUrl: './marketplace.component.html',
   styleUrl: './marketplace.component.scss'
 })
