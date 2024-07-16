5230a110f3b0d1972fce8184c6a7227a1a7256b8
SondreB <sondre@outlook.com>
2024-07-16 17:43:14 +0200

    Add some Discussion cards

--- app/src/app/community/community.component.ts
+++ app/src/app/community/community.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { AgoPipe } from '../shared/pipes/ago.pipe';
 
 @Component({
   selector: 'app-community',
   standalone: true,
-  imports: [MatTabsModule, MatIconModule],
+  imports: [AgoPipe, MatCardModule, MatButtonModule, MatTabsModule, MatIconModule],
   templateUrl: './community.component.html',
   styleUrl: './community.component.scss',
 })
