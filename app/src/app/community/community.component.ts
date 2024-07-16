92ae18e2aea302ed8655c7da3bbd2e251e684d1d
SondreB <sondre@outlook.com>
2024-07-16 17:53:49 +0200

    Add Events mockup

--- app/src/app/community/community.component.ts
+++ app/src/app/community/community.component.ts
@@ -5,11 +5,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { AgoPipe } from '../shared/pipes/ago.pipe';
 import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-community',
   standalone: true,
-  imports: [RouterModule, AgoPipe, MatCardModule, MatButtonModule, MatTabsModule, MatIconModule],
+  imports: [CommonModule, MatListModule, RouterModule, AgoPipe, MatCardModule, MatButtonModule, MatTabsModule, MatIconModule],
   templateUrl: './community.component.html',
   styleUrl: './community.component.scss',
 })
