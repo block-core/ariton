4cfc3def8c9201d2f0835056277a406fbec66dfd
SondreB <sondre@outlook.com>
2024-07-16 17:58:59 +0200

    Add Media mockup

--- app/src/app/community/community.component.ts
+++ app/src/app/community/community.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,4 +15,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './community.component.html',
   styleUrl: './community.component.scss',
 })
-export class CommunityComponent {}
+export class CommunityComponent {
+  images = ['nature', 'sky', 'grass', 'mountains', 'rivers', 'glacier', 'forest', 'streams', 'rain', 'clouds'];
+
+  photos = signal<any[]>([]);
+
+  ngOnInit() {
+    const photos: any[] = [];
+    for (let i = 0; i < this.images.length; i++) {
+      photos.push({
+        id: 'id' + 1,
+        name: `Community ${i + 1}`,
+        description: `This is a description of community. We are a great community with many members.`,
+        thumbnail: `https://picsum.photos/seed/${this.images[i]}x/200/300`,
+        private: false,
+        visibility: 'public',
+        type: 'generic',
+        features: {
+          discussion: true,
+          members: true,
+          events: true,
+          media: true,
+          files: true,
+        },
+        apps: ['events', 'media', 'files'],
+      });
+    }
+
+    this.photos.set(photos);
+  }
+}
