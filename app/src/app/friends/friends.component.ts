ad82ff31fe2c3b58d3275130b90794b042ebe049
SondreB <sondre@outlook.com>
2024-07-29 00:42:38 +0200

    Add Friend Requests mockup

--- app/src/app/friends/friends.component.ts
+++ app/src/app/friends/friends.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './friends.component.html',
-  styleUrl: './friends.component.scss'
+  styleUrl: './friends.component.scss',
 })
 export class FriendsComponent {
+  requests = signal<any[]>([]);
 
+  accept(did: string) {}
+
+  reject(did: string) {}
+
+  ngOnInit() {
+    this.requests.set([
+      {
+        name: 'Lu',
+        thumbnail: 'https://ariton.app/assets/lu.jpg',
+      },
+    ]);
+  }
 }
