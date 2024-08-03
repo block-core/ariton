59c7d209695611cd186bdc817f6d137316df8378
SondreB <sondre@outlook.com>
2024-08-03 17:01:06 +0200

    Add ability to set labels
    - Unfortunately the tags cannot be written in a record update, so need to find a different solution or the DWN SDK will add support for updating tags.

--- app/src/app/apps/app/notes/notes.component.ts
+++ app/src/app/apps/app/notes/notes.component.ts
@@ -1,6 +1,6 @@
-import { Component, effect, inject, model, signal } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, effect, inject, model, QueryList, signal, ViewChild, ViewChildren } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,7 +10,7 @@ import { OnDestroy } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule, DatePipe } from '@angular/common';
 import { AgoPipe } from '../../../shared/pipes/ago.pipe';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -19,6 +19,20 @@ import { DialogData, NoteDialogComponent } from './note-dialog.component';
 import { protocolDefinition as noteDefinition } from '../../../../protocols/note';
 import { IdentityService } from '../../../identity.service';
 import { AppService } from '../../../app.service';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { OverlayModule } from '@angular/cdk/overlay';
+
+import {
+  CdkMenuItemRadio,
+  CdkMenuItemCheckbox,
+  CdkMenuGroup,
+  CdkMenu,
+  CdkMenuTrigger,
+  CdkMenuItem,
+  CdkMenuBar,
+  CdkMenuModule,
+} from '@angular/cdk/menu';
 
 export interface Section {
   id: string;
@@ -33,6 +47,7 @@ export interface Section {
   standalone: true,
   imports: [
     CommonModule,
+    MatSelectModule,
     MatDialogModule,
     MatTooltipModule,
     MatCardModule,
@@ -48,14 +63,115 @@ export interface Section {
     MatButtonToggleModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    OverlayModule,
+    CdkMenuModule,
+    CdkMenuBar,
+    CdkMenuItem,
+    CdkMenuTrigger,
+    CdkMenu,
+    CdkMenuGroup,
+    CdkMenuItemCheckbox,
+    CdkMenuItemRadio,
   ],
   templateUrl: './notes.component.html',
   styleUrl: './notes.component.scss',
 })
 export class NotesComponent implements OnDestroy {
+  @ViewChild('matMenuTrigger', { read: MatMenuTrigger })
+  private matMenuTriggerRef!: MatMenuTrigger;
+  @ViewChild('matMenuTrigger', { read: MatButton })
+  private matButtonRef!: MatButton;
+  @ViewChildren('menuItems')
+  private menuItemsRef!: QueryList<MatCheckbox>;
+
+  public triggerButtonText = 'Select a vehicle';
+
+  public premiumAutomobilesList = [
+    { title: 'Audi', activated: false, value: 'audi' },
+    { title: 'Infiniti', activated: false, value: 'infiniti' },
+    { title: 'BMW', activated: false, value: 'bmw' },
+    { title: 'Mercedes', activated: false, value: 'mercedes' },
+    { title: 'Lexus', activated: false, value: 'lexus' },
+    { title: 'Alfa Romeo', activated: false, value: 'alfa romeo' },
+    { title: 'Porsche', activated: false, value: 'porsche' },
+  ];
+  private selectedVehicles: string[] = [];
+  public formattedSelectedVehicles?: string;
+
+  isOpen = false;
+  // public onMenuOpened() {
+  //   this.setFocusOnFirstItem();
+  //   this.triggerButtonText = 'Close menu';
+  //   this.formattedSelectedVehicles = '';
+  // }
+
+  // public onMenuClosed() {
+  //   this.matButtonRef.focus();
+  //   this.triggerButtonText = 'Select a vehicle';
+
+  //   this.formattedSelectedVehicles =
+  //     (this.selectedVehicles.length === 0
+  //       ? 'No vehicles selected'
+  //       : 'You selected ' + this.selectedVehicles.join(', ')) + '.';
+  // }
+
+  async closeLabelsMenu(entry: any) {
+    entry.open = false;
+    console.log('Menu closed:', entry);
+    await this.saveNote(entry, entry.data);
+  }
+
+  public onMenuKeyDown(event: KeyboardEvent, index: number) {
+    switch (event.key) {
+      case 'ArrowUp':
+        if (index > 0) {
+          this.setCheckboxFocus(index - 1);
+        } else {
+          this.menuItemsRef.last.focus();
+        }
+        break;
+      case 'ArrowDown':
+        if (index !== this.menuItemsRef.length - 1) {
+          this.setCheckboxFocus(index + 1);
+        } else {
+          this.setFocusOnFirstItem();
+        }
+        break;
+      case 'Enter':
+        event.preventDefault();
+        this.premiumAutomobilesList[index].activated = !this.premiumAutomobilesList[index].activated;
+        this.onVehicleSelect();
+        setTimeout(() => this.matMenuTriggerRef.closeMenu(), 200);
+        break;
+    }
+  }
+
+  public onVehicleSelect() {
+    this.selectedVehicles = this.premiumAutomobilesList
+      .filter((menuitem) => menuitem.activated)
+      .map((menuitem) => menuitem.title);
+  }
+
+  private setFocusOnFirstItem(): void {
+    const firstCheckbox = this.menuItemsRef.first;
+    firstCheckbox.focus();
+    firstCheckbox._elementRef.nativeElement.classList.add('cdk-keyboard-focused');
+  }
+
+  private setCheckboxFocus(index: number) {
+    const checkBox = this.menuItemsRef.get(index);
+    if (checkBox) {
+      checkBox.focus();
+    }
+  }
+
   viewStyle = model<string>('card');
 
   selectedTags: string[] = [];
+
+  bold = false;
+
+  bold2 = false;
 
   dialog = inject(MatDialog);
 
@@ -64,6 +180,8 @@ export class NotesComponent implements OnDestroy {
   identity = inject(IdentityService);
 
   app = inject(AppService);
+
+  toppings = new FormControl('');
 
   labels = signal<string[]>(['Reminders', 'Inspiration', 'Personal', 'Work']);
 
@@ -75,6 +193,10 @@ export class NotesComponent implements OnDestroy {
   };
 
   records = signal<any[]>([]);
+
+  checkedColumns: any = [];
+
+  columnsCopy: any = [{ key: 1, title: 'test' }];
 
   constructor() {
     // this.layout.disableScrolling();
@@ -99,7 +221,18 @@ export class NotesComponent implements OnDestroy {
         await this.loadNotes();
       }
     });
+
+    // this.toppings.valueChanges.subscribe((tags) => {
+    //   console.log('tags:', tags);
+    //   // this.onSelectionChange(tags);
+    // });
   }
+
+  // async onMenuClose(entry: any) {
+  //   console.log('Menu closed:', entry);
+  //   console.log('Saving updated note');
+  //   await this.saveNote(entry, entry.data);
+  // }
 
   async deleteNote(entry: any) {
     const { status } = await entry.record.delete();
@@ -110,6 +243,8 @@ export class NotesComponent implements OnDestroy {
   }
 
   async loadNotes(tags?: any) {
+    console.log('VALUE OF TAGS:', tags);
+
     var { records } = await this.identity.web5.dwn.records.query({
       message: {
         filter: {
@@ -137,9 +272,10 @@ export class NotesComponent implements OnDestroy {
         // }
 
         this.records.update((records) => [...records, json]);
-        console.log('All requests:', this.records());
       }
     }
+
+    console.log('All requests:', this.records());
   }
 
   onSelectionChange(event: any) {
@@ -150,17 +286,18 @@ export class NotesComponent implements OnDestroy {
     //   return { [this.labelMap[tag]]: tag };
     // });
 
-    const formattedTags: { [key: string]: string } = this.selectedTags.reduce((acc: { [key: string]: string }, tag) => {
-      acc[this.labelMap[tag]] = tag;
-      return acc;
-    }, {});
+    // const formattedTags: { [key: string]: string } = this.selectedTags.reduce((acc: { [key: string]: string }, tag) => {
+    //   acc[this.labelMap[tag]] = tag;
+    //   return acc;
+    // }, {});
 
-    console.log(formattedTags);
+    // console.log(formattedTags);
 
     if (!this.selectedTags || this.selectedTags.length == 0) {
       this.loadNotes();
     } else {
-      this.loadNotes(formattedTags);
+      const tagsString = this.selectedTags.join(',');
+      this.loadNotes({ labels: tagsString });
     }
   }
 
@@ -169,11 +306,13 @@ export class NotesComponent implements OnDestroy {
   }
 
   copyNote(entry: any) {
+    const clonedData = JSON.parse(JSON.stringify(entry.data));
+
     this.saveNote(
       {
-        data: entry.data,
+        data: clonedData,
       },
-      entry.data,
+      clonedData,
     );
   }
 
@@ -234,6 +373,11 @@ export class NotesComponent implements OnDestroy {
     if (entry.record) {
       const { status, record } = await entry.record.update({
         data: data,
+        message: {
+          tags: {
+            labels: data.labels,
+          },
+        },
       });
 
       console.log('Record created:', record);
@@ -253,8 +397,7 @@ export class NotesComponent implements OnDestroy {
         data: data,
         message: {
           tags: {
-            label2: 'Inspiration',
-            label3: 'Personal',
+            labels: data.labels,
           },
           protocol: noteDefinition.protocol,
           protocolPath: 'note',
