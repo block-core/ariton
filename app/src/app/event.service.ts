65ca11e9d24cb27d777fe697888b65c870b39739
SondreB <sondre@outlook.com>
2024-09-09 20:19:29 +0200

    Update event.service.ts

--- app/src/app/event.service.ts
+++ app/src/app/event.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ConnectionService } from './connection.service';
+import { IdentityService } from './identity.service';
+import { Record } from '@web5/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventService {
+  connections = inject(ConnectionService);
 
-  constructor() { }
+  identity = inject(IdentityService);
+
+  constructor() {}
+
+  /** Call to setup event listeners and orchestration of data updates. */
+  async initialize() {
+    // const subscriptionHandler = (record: Record) => {
+    //   console.log('received', record);
+    // };
+    // const { status } = await this.identity.web5.dwn.records.query({
+    //   from: 'did:example:bob',
+    //   message: {
+    //     filter: {
+    //       protocol: 'https://schema.org/protocols/social',
+    //     },
+    //   },
+    //   subscriptionHandler,
+    // });
+  }
 }
