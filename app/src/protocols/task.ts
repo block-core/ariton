ceb8bd336e0824293a7552022969841df7f8f462
SondreB <sondre@outlook.com>
2024-09-09 20:00:40 +0200

    Add query and subscribe to collaborator role

--- app/src/protocols/task.ts
+++ app/src/protocols/task.ts
@@ -20,11 +20,14 @@ export const protocolDefinition = {
       $role: true,
     },
     list: {
-      $actions: [{ role: 'collaborator', can: ['read'] }],
+      $actions: [{ role: 'collaborator', can: ['read', 'query', 'subscribe'] }],
       task: {
         $actions: [
           // Allow the collaborator to do anything with the items within the list, but not delete/edit entire list.
-          { role: 'collaborator', can: ['create', 'update', 'read', 'delete', 'co-update', 'co-delete'] },
+          {
+            role: 'collaborator',
+            can: ['create', 'update', 'read', 'delete', 'query', 'subscribe', 'co-update', 'co-delete'],
+          },
         ],
       },
     },
