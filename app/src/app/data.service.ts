c31a3ee1809f276db0060d7b70678f13444e5f8a
SondreB <sondre@outlook.com>
2024-09-19 13:45:18 +0200

    Add a Data Service
    - This service will encapsulate communication with the DWN and Web5 SDK.

---
+++ app/src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { effect, inject, Injectable } from '@angular/core';
+import { IdentityService } from './identity.service';
+import { AppService } from './app.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  identity = inject(IdentityService);
+
+  app = inject(AppService);
+
+  constructor() {
+    effect(async () => {
+      if (this.app.initialized()) {
+        await this.initialize();
+      }
+    });
+  }
+
+  async initialize() {}
+}
