a9f2f89a9887869770d0c55eb876caacce7256dc
SondreB <sondre@outlook.com>
2024-09-24 20:15:55 +0200

    Improve the custom title bar

--- app/src/app/apps/app/player/controls/controls.component.ts
+++ app/src/app/apps/app/player/controls/controls.component.ts
@@ -1,17 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { LayoutService } from '../../../../layout.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-player-controls',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './controls.component.html',
   styleUrl: './controls.component.scss',
 })
 export class PlayerControlsComponent {
+  @Input() miniplayer: boolean = false;
   layout = inject(LayoutService);
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   if (changes['miniplayer']) {
+  //     if (this.miniplayer) {
+  //       this.renderer.addClass(this.el.nativeElement, 'miniplayer');
+  //     } else {
+  //       this.renderer.removeClass(this.el.nativeElement, 'miniplayer');
+  //     }
+  //   }
+
+  //   console.log('CHANGES', changes);
+  // }
 
   closeTray() {
     this.layout.tray.set(false);
