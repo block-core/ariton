81ca75a2c0300946f207394fa35919d95fc8637b
SondreB <sondre@outlook.com>
2024-10-02 13:04:01 +0200

    Add margin on notification settings

--- app/src/app/settings/notifications/notifications.component.ts
+++ app/src/app/settings/notifications/notifications.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LayoutService } from '../../layout.service';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
   imports: [],
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.scss'
+  styleUrl: './notifications.component.scss',
 })
 export class NotificationsComponent {
+  layout = inject(LayoutService);
 
+  constructor() {
+    this.layout.marginOn();
+  }
 }
