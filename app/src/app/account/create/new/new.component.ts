80384749a1b1b96345ba706d1412d7a45e6520df
SondreB <sondre@outlook.com>
2024-10-16 20:43:18 +0200

    Implement account creation

--- app/src/app/account/create/new/new.component.ts
+++ app/src/app/account/create/new/new.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Web5IdentityAgent } from '@web5/identity-agent';
 import { Web5 } from '@web5/api';
 import { IdentityService } from '../../../identity.service';
+import { DidDht } from '@web5/dids';
 
 @Component({
   selector: 'app-new',
@@ -12,11 +13,31 @@ import { IdentityService } from '../../../identity.service';
   styleUrl: './new.component.scss',
 })
 export class NewComponent {
-  constructor(private identityService: IdentityService) {}
+  identity = inject(IdentityService);
+
+  constructor() {}
 
   ngOnInit() {}
 
   async createAccount() {
+    console.log('Creating account...');
+
+    const agent = this.identity.activeAgent();
+
+    console.log('Create...');
+    const bearerIdentity = await agent.identity.create({ metadata: { name: 'My new account' } });
+
+    console.log('Export...');
+    const portableIdentity = await bearerIdentity.export();
+
+    // console.log('Import...');
+    // const bearerIdentity1 = await agent.identity.import({ portableIdentity });
+
+    console.log('Manage...');
+    const bearerIdentity2 = await agent.identity.manage({ portableIdentity: portableIdentity });
+
+    // const DidDht.create();
+
     //  const res = await Web5.connect({ sync: '5s' });
 
     //console.log('Web5 Connect:', res.did);
@@ -27,53 +48,53 @@ export class NewComponent {
     
 */
     //const agent = await Web5IdentityAgent.create();
-    const password = 'insecure-static-phrase';
+    // const password = 'insecure-static-phrase';
 
-    const recoveryPhrase1 = 'rubber fresh pluck lion upper length scene spawn fruit place spin glare';
-    const recoveryPhrase2 = 'pistol base actress injury barely ivory poet happy raise tell idea summer';
-    const recoveryPhrase3 = 'stool trap result elevator pass safe tourist planet ranch black satoshi text';
+    // const recoveryPhrase1 = 'rubber fresh pluck lion upper length scene spawn fruit place spin glare';
+    // const recoveryPhrase2 = 'pistol base actress injury barely ivory poet happy raise tell idea summer';
+    // const recoveryPhrase3 = 'stool trap result elevator pass safe tourist planet ranch black satoshi text';
 
-    console.log('Creating agents...');
+    // console.log('Creating agents...');
 
-    const { web5, did: userDid } = await Web5.connect({ sync: '15s', recoveryPhrase: recoveryPhrase1 });
+    // const { web5, did: userDid } = await Web5.connect({ sync: '15s', recoveryPhrase: recoveryPhrase1 });
 
-    console.log(userDid);
-    console.log(web5);
+    // console.log(userDid);
+    // console.log(web5);
 
-    const agentDidExported = await web5.agent.agentDid.export();
-    console.log(agentDidExported);
+    // const agentDidExported = await web5.agent.agentDid.export();
+    // console.log(agentDidExported);
 
-    const agent = web5.agent as Web5IdentityAgent;
+    // const agent = web5.agent as Web5IdentityAgent;
 
-    const list = await agent.identity.list();
+    // const list = await agent.identity.list();
 
-    console.log(list);
-    const did = list[0];
+    // console.log(list);
+    // const did = list[0];
 
-    console.log(did.metadata);
-    did.metadata.name = 'John Doe';
-    const portableDid = await did.export();
+    // console.log(did.metadata);
+    // did.metadata.name = 'John Doe';
+    // const portableDid = await did.export();
 
-    console.log('DIDS:');
-    console.log(portableDid);
-    //console.log(JSON.stringify(portableDid));
+    // console.log('DIDS:');
+    // console.log(portableDid);
+    // //console.log(JSON.stringify(portableDid));
 
-    const portableAgentDid = await agent.agentDid.export();
-    console.log(portableAgentDid);
-    //const didExported = await web5.agent.
+    // const portableAgentDid = await agent.agentDid.export();
+    // console.log(portableAgentDid);
+    // //const didExported = await web5.agent.
 
-    console.log(JSON.stringify(portableDid.portableDid));
-    console.log(JSON.stringify(portableAgentDid));
+    // console.log(JSON.stringify(portableDid.portableDid));
+    // console.log(JSON.stringify(portableAgentDid));
 
-    // await this.identityService.initialize(password, recoveryPhrase1, 'DATA/AGENT1');
-    // await this.identityService.initialize(password, recoveryPhrase2, 'DATA/AGENT2');
-    //await this.identityService.initialize(password, recoveryPhrase3, 'DATA/AGENT3');
+    // // await this.identityService.initialize(password, recoveryPhrase1, 'DATA/AGENT1');
+    // // await this.identityService.initialize(password, recoveryPhrase2, 'DATA/AGENT2');
+    // //await this.identityService.initialize(password, recoveryPhrase3, 'DATA/AGENT3');
 
-    console.log('Agents created.');
-    console.log(this.identityService.agents());
+    // console.log('Agents created.');
+    // console.log(this.identityService.agents());
 
-    console.log(await this.identityService.agents()[0].identity.list());
-    console.log(await this.identityService.agents()[1].identity.list());
+    // console.log(await this.identityService.agents()[0].identity.list());
+    // console.log(await this.identityService.agents()[1].identity.list());
 
     /*
     let recoveryPhrase = undefined;
