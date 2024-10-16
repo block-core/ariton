9d17d64846915d3270298ba67b69c4534c1ee720
SondreB <sondre@outlook.com>
2024-10-16 21:21:17 +0200

    Add ability to backup account

--- app/src/app/account/account.component.ts
+++ app/src/app/account/account.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { IdentityService } from '../identity.service';
+import { LayoutService } from '../layout.service';
+import { AppService } from '../app.service';
+import { BearerIdentity } from '@web5/agent';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
-    selector: 'app-account',
-    standalone: true,
-    imports: [MatCardModule, MatButtonModule, RouterLink],
-    templateUrl: './account.component.html',
-    styleUrl: './account.component.scss',
+  selector: 'app-account',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, RouterLink, MatTooltipModule],
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.scss',
 })
-export class AccountComponent {}
+export class AccountComponent {
+  identity = inject(IdentityService);
+
+  layout = inject(LayoutService);
+
+  app = inject(AppService);
+
+  currentIdentity: BearerIdentity | undefined;
+
+  constructor(private route: ActivatedRoute) {
+    this.layout.marginOn();
+    effect(async () => {
+      if (this.app.initialized()) {
+        this.route.params.subscribe(async (params) => {
+          //   const source = params['source'];
+          const id = params['id'];
+
+          const identity = await this.identity.activeAgent().identity.get({ didUri: id });
+          this.currentIdentity = identity;
+
+          //   console.log(this.registryService);
+
+          //   this.entry = this.registryService.bsn.accounts.find((account) => {
+          //     return account.id === id;
+          //   });
+
+          //   console.log(this.entry);
+        });
+      }
+    });
+  }
+
+  async backupAccount() {
+    const portableIdentity = await this.currentIdentity?.export();
+    if (portableIdentity) {
+      const blob = new Blob([JSON.stringify(portableIdentity)], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'portableIdentity.json';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    }
+  }
+
+  deleteAccount() {}
+}
