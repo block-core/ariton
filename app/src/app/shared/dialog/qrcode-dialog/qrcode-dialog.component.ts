89951cbc2561b32075586373fca601baa311534a
SondreB <sondre@outlook.com>
2024-10-24 12:08:01 +0200

    Increase the QR and add option

--- app/src/app/shared/dialog/qrcode-dialog/qrcode-dialog.component.ts
+++ app/src/app/shared/dialog/qrcode-dialog/qrcode-dialog.component.ts
@@ -1,23 +1,45 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import * as QRCode from 'qrcode';
 
 @Component({
   selector: 'app-qrcode-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, MatButtonToggleModule, FormsModule],
   templateUrl: './qrcode-dialog.component.html',
   styleUrl: './qrcode-dialog.component.scss',
 })
 export class QRCodeDialogComponent {
+  qrStyle = 'did';
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: { did: string }) {}
 
   ngAfterViewInit() {
+    this.generateQR(this.data.did);
+  }
+
+  generateQR(data: string) {
     const canvas = document.querySelector('canvas');
-    QRCode.toCanvas(canvas, this.data.did, (error: any) => {
+    QRCode.toCanvas(canvas, data, { width: 256 }, (error: any) => {
       if (error) {
         console.error('Error generating QR code: ', error);
       }
     });
+  }
+
+  onToggleGroupChange(event: MatButtonToggleChange) {
+    // Handle the change event
+    console.log('Selected value:', event.value);
+    console.log(this.qrStyle);
+    // Add your logic here
+
+    if (this.qrStyle == 'profile') {
+      this.generateQR(`https://profile.ariton.app/?did=${this.data.did}`);
+    } else {
+      this.generateQR(this.data.did);
+    }
   }
 }
